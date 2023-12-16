7cebde611e6bfe3597ca8177362d0902fd2f9140
dima_samborskyi <samborskiydmitriy1@gmail.com>
2023-12-16 13:50:18 +0200

    some fixes

--- actions/getData.js
+++ actions/getData.js
@@ -1,7 +1,7 @@
 
 export async function getData(url) {
     const localhost = process.env.NEXT_PUBLIC_API_LOCALHOST
-    const response = await fetch(localhost + url)
+    const response = await fetch(localhost + url, {cache: 'no-store'})
     
     const data = await response.json();
     return data
